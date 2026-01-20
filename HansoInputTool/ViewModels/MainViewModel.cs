08d270b3d50ca827ec18178278a52e618d6de310
Sedil Bloodley <sedil.bloodley@outlook.com>
2026-01-20 19:21:16 +0900

    リビルドヴァージョンの更新

--- HansoInputTool/ViewModels/MainViewModel.cs
+++ HansoInputTool/ViewModels/MainViewModel.cs
@@ -28,7 +28,7 @@ namespace HansoInputTool.ViewModels
 
         #region Constants and Paths
         private const string AppName = "HansoInputTool";
-        private const string CurrentVersion = "1.6.3";
+        private const string CurrentVersion = "1.5.3";
         private const string GithubToken = "";
         private const string VersionInfoUrl = "https://raw.githubusercontent.com/ligdoor/HansoInputTool/refs/heads/master/version.json";
         private const string ReleasesPageUrl = "https://github.com/ligdoor/HansoInputTool/releases";
